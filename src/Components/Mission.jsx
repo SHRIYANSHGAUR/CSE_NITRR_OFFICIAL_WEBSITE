8bcf60f436337ddc3e26ee4394ce3cd594aa5c1a
SinghAman90 <94293803+SinghAman90@users.noreply.github.com>
2023-01-17 22:56:13 +0530

    Update Mission.jsx

--- src/Components/Mission.jsx
+++ src/Components/Mission.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import './cstyle.css';
+import React from "react";
+import "./cstyle.css";
 
 function Mission() {
   return (
     <div id="mission" class="section-cotent">
       <div class="container">
         <div class="title-section text-center mission1">
-          <h2>Word from H.O.D.</h2>
+          <h2>About H.O.D.</h2>
           <span></span>
         </div>
         <div class="row">
@@ -16,18 +16,23 @@ function Mission() {
               src="https://i.postimg.cc/fVdbRHgW/img1.png"
               alt="React Image"
             />
-
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis,
-              ex, amet, quisquam magni quasi modi sint accusamus architecto
-              velit veritatis nobis autem repellat dolore quis atque totam
-              laudantium ab sed impedit beatae esse error culpa voluptatem eius
-              et. <br></br>
-              Aut, nulla, debitis voluptates doloribus quisquam maiores
-              repudiandae nam culpa voluptatibus alias earum magnam numquam.
-              Consectetur, ratione, ipsam totam et nesciunt atque temporibus
-              fuga quos rem deserunt tempore dolore eaque dolorum a doloremque
-              optio nihil pariatur aliquid ex id officiis aliquam sed.
+              Dr. Dilip Singh Sisodia is Working as Associate Professor at the
+              Department of Computer Science and Engineering, National Institute
+              of Technology Raipur, Raipur, India.Dr. Sisodia have expertise in
+              various fields of computer science including Artificial
+              Intelligence, Machine Learning, Soft Computing, Computation
+              intelligence, Theoretical computer science and many more.
+              <br></br>
+              Dr. Sisodia has contributed to more than 100 refereed articles,
+              including 78 papers in Web of Science/Scopus indexed journals, his
+              conference proceedings and book chapters have been published by
+              reputed international publishers.Some notable publication
+              destinations of Dr. Sisodia are IEEE Transactions on Systems: Man,
+              and Cybernetics Systems, Knowledge base systems, Expert system,
+              and applications, Information Sciences and more. He has also
+              shared his work at various international conferences in countries
+              like Israel, Spain, Singapore, Thailand, and more.
             </p>
           </div>
         </div>
@@ -40,16 +45,13 @@ function Mission() {
         <div class="row">
           <div class="col-md-13">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis,
-              ex, amet, quisquam magni quasi modi sint accusamus architecto
-              velit veritatis nobis autem repellat dolore quis atque totam
-              laudantium ab sed impedit beatae esse error culpa voluptatem eius
-              et. <br></br>
-              Aut, nulla, debitis voluptates doloribus quisquam maiores
-              repudiandae nam culpa voluptatibus alias earum magnam numquam.
-              Consectetur, ratione, ipsam totam et nesciunt atque temporibus
-              fuga quos rem deserunt tempore dolore eaque dolorum a doloremque
-              optio nihil pariatur aliquid ex id officiis aliquam sed.
+              To impart effective education at undergraduate levels in the field
+              of Computer Science and Engineering. To strive steadily for the
+              overall growth and development of intellect and personality of the
+              students who, as professionals, would be confident to face the
+              challenges of the world of work. To conduct Research and
+              Development and offer consultancy services. To improve
+              continuously the quality of teaching-learning process.
             </p>
           </div>
         </div>
@@ -62,16 +64,12 @@ function Mission() {
         <div class="row">
           <div class="col-md-13">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis,
-              ex, amet, quisquam magni quasi modi sint accusamus architecto
-              velit veritatis nobis autem repellat dolore quis atque totam
-              laudantium ab sed impedit beatae esse error culpa voluptatem eius
-              et. <br></br>
-              Aut, nulla, debitis voluptates doloribus quisquam maiores
-              repudiandae nam culpa voluptatibus alias earum magnam numquam.
-              Consectetur, ratione, ipsam totam et nesciunt atque temporibus
-              fuga quos rem deserunt tempore dolore eaque dolorum a doloremque
-              optio nihil pariatur aliquid ex id officiis aliquam sed.
+              To promote Research and Development in the frontier areas of
+              Computer Science & Engineering. To generate Competent
+              Professionals to become part of the industry and Research
+              Organizations at the National and International levels. To provide
+              necessary strengths to enable the students to Innovate and to
+              become Entrepreneurs.
             </p>
           </div>
         </div>
