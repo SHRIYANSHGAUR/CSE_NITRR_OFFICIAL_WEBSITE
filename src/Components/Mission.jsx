81dbcca1f71e4ac0b05b566d134fa5f2e8c98ccc
chiruvishal <86530058+chiruvishal@users.noreply.github.com>
2023-01-19 01:20:30 +0530

    Update mission and owl

--- src/Components/Mission.jsx
+++ src/Components/Mission.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Row, Col} from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import './cstyle.css';
-
 
 function Mission() {
   return (
@@ -11,17 +10,17 @@ function Mission() {
           <h2>Word from H.O.D.</h2>
           <span></span>
         </div>
-        <Row >
+        <Row>
           <Col sm={12} md={2}>
             <img
               class="hod"
               src="https://i.postimg.cc/fVdbRHgW/img1.png"
               alt="React Image"
             />
-            </Col>
-            <Col sm={12} md={10}>
-            <p>
-            Dr. Dilip Singh Sisodia is Working as Associate Professor at the
+          </Col>
+          <Col sm={12} md={10}>
+            <p className="p2">
+              Dr. Dilip Singh Sisodia is Working as Associate Professor at the
               Department of Computer Science and Engineering, National Institute
               of Technology Raipur, Raipur, India.Dr. Sisodia have expertise in
               various fields of computer science including Artificial
@@ -38,55 +37,55 @@ function Mission() {
               shared his work at various international conferences in countries
               like Israel, Spain, Singapore, Thailand, and more.
             </p>
-            </Col>
+          </Col>
         </Row>
       </div>
       <Row>
-      <Col xs={12} md={6}>
-      <div class="container">
-        <div class="mission1-section text-center mission1">
-          <h2>Mission</h2>
-          <span></span>
-        </div>
-        <div class="row">
-          <div class="col-md-13">
-            <p className='content2'>
-            To impart effective education at undergraduate levels in the field
-              of Computer Science and Engineering. To strive steadily for the
-              overall growth and development of intellect and personality of the
-              students who, as professionals, would be confident to face the
-              challenges of the world of work. To conduct Research and
-              Development and offer consultancy services. To improve
-              continuously the quality of teaching-learning process.
-            </p>
+        <Col xs={12} md={6}>
+          <div class="container">
+            <div class="mission1-section text-center mission1">
+              <h2>Mission</h2>
+              <span></span>
+            </div>
+            <div class="row">
+              <div class="col-md-13">
+                <p className="content2 p2">
+                  To impart effective education at undergraduate levels in the
+                  field of Computer Science and Engineering. To strive steadily
+                  for the overall growth and development of intellect and
+                  personality of the students who, as professionals, would be
+                  confident to face the challenges of the world of work. To
+                  conduct Research and Development and offer consultancy
+                  services. To improve continuously the quality of
+                  teaching-learning process.
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      </Col>
-      <Col xs={12} md={6}>
-      <div class="container">
-        <div class="mission1-section text-center mission1">
-          <h2>Vision</h2>
-          <span></span>
-        </div>
-        <div class="row">
-          <div class="col-md-13">
-            <p>
-            To promote Research and Development in the frontier areas of
-              Computer Science & Engineering. To generate Competent
-              Professionals to become part of the industry and Research
-              Organizations at the National and International levels. To provide
-              necessary strengths to enable the students to Innovate and to
-              become Entrepreneurs.
-            </p>
+        </Col>
+        <Col xs={12} md={6}>
+          <div class="container">
+            <div class="mission1-section text-center mission1">
+              <h2>Vision</h2>
+              <span></span>
+            </div>
+            <div class="row">
+              <div class="col-md-13">
+                <p className="p2">
+                  To promote Research and Development in the frontier areas of
+                  Computer Science & Engineering. To generate Competent
+                  Professionals to become part of the industry and Research
+                  Organizations at the National and International levels. To
+                  provide necessary strengths to enable the students to Innovate
+                  and to become Entrepreneurs.
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      </Col>
+        </Col>
       </Row>
     </div>
   );
 }
 
 export default Mission;
-
